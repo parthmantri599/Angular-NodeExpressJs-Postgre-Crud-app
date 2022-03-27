57836ff1c22d1c8df868479508c4790e34f23188
parthmantri599 <46102141+parthmantri599@users.noreply.github.com>
2022-03-28 00:08:44 +0530

    Add files via upload

---
+++ Node-Express_Backend/app/config/db.config.js
@@ -0,0 +1,37 @@
+const env = require("./env.js");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  env.DB,
+  env.USER,
+  env.PASSWORD,
+  {
+    host: env.HOST,
+    dialect: env.dialect,
+    operatorsAliases: false,
+    pool: {
+      max: env.max,
+      min: env.pool.min,
+      acquire: env.pool.acquire,
+      idle: env.pool.idle
+    }
+  }
+);
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.Customer = require('../models/customer.model.js')(sequelize, Sequelize);
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId"
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId"
+});
+db.ROLES = ["user", "admin"];
+
+module.exports = db;
---
+++ Node-Express_Backend/app/config/env.js
@@ -0,0 +1,15 @@
+const env = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "1234",
+    DB: "testdb",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
+   
+  module.exports = env;
